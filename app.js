c827de24fea9731f0d763f80d57599aa3bbe6121
Atul Kumar <ramkanaujia63@gmail.com>
2026-08-04 21:01:03 +0530

    Minor changes

--- app.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".unit-btn");
-const dropdown = document.querySelector(".unit-dropdown");
-
-btn.addEventListener("click", () => {
-  dropdown.classList.toggle("show");
-});
